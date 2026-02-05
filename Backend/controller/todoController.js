479161d794039e3f8ef369276e8b7789579b1165
nandu <pallanandhine7777@gmail.com>
2026-02-05 10:01:24 +0530

    mongodb

--- Backend/controller/todoController.js
+++ Backend/controller/todoController.js
@@ -1,10 +1,27 @@
-let todos=[];
-let id=1;
-exports.getTodo=(req,res)=>{
-    res.status(200).json(todos)
+// let todos=[];
+// let id=1;
+const Todo=require("../models/todo");
+exports.getTodo=async(req,res)=>{
+    try{
+       const todos= await Todo.find();
+       res.status(200).json(todos);
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
 };
-exports.createTodo=(req,res)=>{
+exports.createTodo=async(req,res)=>{
+    try{
     const {task} =req.body;
+    if(task===undefined)
+        return res.status(401).json({message:"task not found"});
+    const todos =await Todo.create({
+        task,completed:false
+    })
+    res.status(201).json(todos);
+}catch(err){
+     res.status(500).send(err);
+}
     const newTodo={
         id:id++,
         task,
@@ -14,21 +31,27 @@ exports.createTodo=(req,res)=>{
     res.json(newTodo)
 };
 
-exports.updateTodo=(req,res)=>{
-    const todo=todos.find((t)=>t.id===parseInt(req.params.id));
-    console.log(todo);
+exports.updateTodo=async(req,res)=>{
+   try{
+     const todo=await Todo.findById(req.params.id);
     if(!todo){
-        res.json({message:"todo not found"})
+        res.status(401).json({message:"todo not found"})
     }
     todo.task=req.body.task || todo.task;
     todo.completed=req.body.completed===undefined?todo.completed:req.body.completed;
-    res.json(todo)
+    await todo.save();
+    res.status(200).json(todo);
+   }
+   catch(err){
+    res.status(500).send(err);
+   }
     
 };
-exports.deleteTodo=(req,res)=>{
-    const index=todos.findIndex((t)=>t.id===parseInt(req.params.id))
-    if(index===-1)
-        return res.status(404).json({message:"todo not found"});
-    todos=todos.filter((_,i)=>i!==index);
-    res.status(200).json({message:"todo deleted succesfully"})
+exports.deleteTodo=async (req,res)=>{
+try{
+        await Todo.findByIdAndDelete(req.params.id);
+        res.status(200).json({message:"task deleted successfully"});
+}catch(err){
+    res.status(500).send(err);
 }
+};
--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,30 +1,61 @@
-let user = [];
-let id=1;
-exports.createUser=(req,res)=>{
+// let user = [];
+// let id=1;
+const User = require("../models/user")
+exports.createUser=async (req,res)=>{
+   try{
     const {name,email}=req.body;
-    const newUser={
-        id:id++,
+    const exist = await User.findOne({email})
+    if(exist)
+        return res.status(401).json({message:"user already exists"})
+        const user=await User.create({
         name,
         email
-    };
-    user.push(newUser);
-    res.status(200).json(newUser);
+    })
+    res.status(200).json(user);
+}catch(err){
+    res.status(500).send(err)
+}
+    // user.push(newUser);
+    // res.status(200).json(newUser);
 }
 
-exports.getUser=(req,res)=>{
-    res.status(200).json(users);
+exports.getUser=async(req,res)=>{
+    try{
+       const users= await User.find();
+       res.status(200).json(users)
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
 };
-exports.getUserById=(req,res)=>{
-    const myuser=user.find((t)=>t.id===parseInt(req.params.id));
-    if(myuser===undefined)return res.status(404).json({message:"user not found"});
-    res.status(200).json(myuser)
+exports.getUserById=async(req,res)=>{
+try{
+        const user= await User.findById(req.params.id);
+        if(!user)
+            return res.status(401).json({message:"user not found"})
+        res.status(200).json(user);
+}catch(err){
+    res.status(500).json(err);
+}   
 };
-exports.editUser=(req,res)=>{
-    const myuser=user.find((t)=>t.id===parseInt(req.params.id));
-    if(myuser===undefined)
-        return res.status(404).json({message:"user not found"});
-    myuser.name=req.body.name ===undefined?myuser.name:req.body.name;
-    myuser.email=req.body.email||myuser.email;
-    res.status(200).json(myuser)
-};
-    exports.deleteUser=(req,res)=>{}
+exports.editUser=async(req,res)=>{
+    try{
+        const user=await User.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        if(!user) return res.status(404).json({message:"USER not found"}); 
+        res.status(200).json(user) 
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+}
+   
+    exports.deleteUser=async(req,res)=>{
+    try{
+        const user=await User.findOneAndDelete(req.params.id,req.body,{new:true});
+        if(!user) return res.status(404).json({message:"user deleted sucessfully"}); 
+        res.status(200).json(user) 
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+}
